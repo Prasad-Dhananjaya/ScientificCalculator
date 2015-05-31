d3ef8aba3e66915e9d306e1eb07abd08a4fca7ba
Prasad-Dhananjaya <pdhana52@gmail.com>
2015-05-31 14:53:14 +0530

    Test 001

---
+++ Scientific Calculator/Main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: May 31, 2015
+ *      Author: user
+ */
+
+#include "Main.h"
+
+Main::Main() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Main::~Main() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Scientific Calculator/Main.h
@@ -0,0 +1,17 @@
+/*
+ * Main.h
+ *
+ *  Created on: May 31, 2015
+ *      Author: user
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+class Main {
+public:
+	Main();
+	virtual ~Main();
+};
+
+#endif /* MAIN_H_ */
